73bbf1279ed903dad301c3d2b6e2d8320555f1ee
turki alotaibi <turkialotaibi@MacBook-Air-turki.local>
2026-08-16 08:20:39 +0300

    chore: finalize privacy hosting and iOS push configuration

--- lib/config/app_links.dart
+++ lib/config/app_links.dart
@@ -1,5 +1,5 @@
 const String kPrivacyPolicyUrl = String.fromEnvironment(
   'KLOOF_PRIVACY_POLICY_URL',
   defaultValue:
-      'https://github.com/trtalotaibi-boop/kloof/blob/release/mvp-final-2026-08-15/PRIVACY_POLICY.md',
+      'https://kloof-1cfcd.web.app/privacy',
 );
